a1346a5d81605dee26f17ed6fb0a11fb19414810
AjithPG <ajithpg008@gmail.com>
2020-06-04 15:57:07 +0530

    added script to submit book data to storage

---
+++ app.js
@@ -0,0 +1,74 @@
+//Book class:Represent a Book
+class Book{
+    constructor(title,author,isbn){
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+    }
+}
+//UI class :Handle UI tasks
+class UI{
+    static displayBooks(){
+        const StoredBooks = [
+           {
+               title:'Book One',
+               author:'John Doe',
+               isbn:'3434343'
+           },
+           {
+
+               title:'Book two',
+               author:'John Doe',
+               isbn:'454545'
+           }
+        ];
+      const books = StoredBooks;
+
+      books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book){
+        var list = document.querySelector('#book-list');
+        var row = document.createElement('tr');
+
+        row.innerHTML =`
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete"><i class="fas fa-times-circle"></i></a></td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static clearFields(){
+        document.querySelector('#title').value = '';
+        
+        document.querySelector('#author').value = '';
+        
+        document.querySelector('#isbn').value = '';
+    }
+}
+//Store class:Handle Storage
+//Event:Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+//Event:Add a book
+document.querySelector('#book-form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    //Get Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    
+    //Instatiate Book
+    const book = new Book(title,author,isbn);
+
+    //Add books to UI
+    UI.addBookToList(book);
+    
+    //clear field
+    UI.clearFields();
+
+});
+
+//Event:Remove a Book
